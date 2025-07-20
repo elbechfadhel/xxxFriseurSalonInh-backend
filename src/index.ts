a4755a633d2caefbe69e5760140a2bf42f486e04
Bech, Fadhel (EXTERN: Novobit) <extern.fadhel.bech2@volkswagen.de>
2025-07-20 10:43:02 +0200

    fix index file

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import cors from 'cors';
 import reservationRoutes from './routes/reservationRoutes';
 import employeeRoutes from "./routes/employeeRoutes";
 import verifyEmailRoutes from "./routes/verifyEmailRoutes";
-import verifySmsRoutes from "./routes/verifySmsRoutes";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -14,7 +13,7 @@ app.use(express.json());
 app.use('/api/reservations', reservationRoutes);
 app.use('/api/employees', employeeRoutes);
 app.use('/api/verify-email', verifyEmailRoutes);
-app.use('/api/verify-sms', verifySmsRoutes);
+
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
