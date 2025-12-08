6506e781a72d2d99d78594cee547838bae0f29ed
Bech, Fadhel (EXTERN: Novobit) <extern.fadhel.bech2@volkswagen.de>
2025-12-08 19:09:30 +0100

    prisma client for  getAvailabilityForDay

--- src/controllers/availabilityController.ts
+++ src/controllers/availabilityController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { prisma } from '../prisma/client';
+import { prisma } from "../prisma/prisma";
 
 export const getAvailabilityForDay = async (req: Request, res: Response) => {
     try {
